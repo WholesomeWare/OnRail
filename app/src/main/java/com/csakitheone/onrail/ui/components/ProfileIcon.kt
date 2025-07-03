f8bd3f890c0297335f950f0cb2ba79bda344d7f0
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2025-07-03 19:15:48 +0200

    Added link to MÁVINFORM

--- app/src/main/java/com/csakitheone/onrail/ui/components/ProfileIcon.kt
+++ app/src/main/java/com/csakitheone/onrail/ui/components/ProfileIcon.kt
@@ -11,18 +11,22 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowRight
 import androidx.compose.material.icons.automirrored.filled.Login
 import androidx.compose.material.icons.automirrored.filled.Logout
+import androidx.compose.material.icons.automirrored.filled.OpenInNew
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.Bookmarks
 import androidx.compose.material.icons.filled.GpsFixed
 import androidx.compose.material.icons.filled.History
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.OpenInNew
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
@@ -46,6 +50,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -73,6 +78,7 @@ fun ProfileIcon(
 ) {
     val coroutineScope = rememberCoroutineScope()
     val activity = LocalActivity.current
+    val colorScheme = MaterialTheme.colorScheme
 
     var isGreetingEnabled by remember { mutableStateOf(false) }
     var greetingText by remember { mutableStateOf("") }
@@ -171,6 +177,33 @@ fun ProfileIcon(
 
                     extraDropdownMenuItems { isMenuOpen = false }
                 }
+                DropdownMenuItem(
+                    onClick = {
+                        if (activity == null) return@DropdownMenuItem
+
+                        CustomTabsIntent.Builder()
+                            .setDefaultColorSchemeParams(
+                                CustomTabColorSchemeParams.Builder()
+                                    .setToolbarColor(colorScheme.primary.toArgb())
+                                    .setSecondaryToolbarColor(colorScheme.secondary.toArgb())
+                                    .build()
+                            )
+                            .build()
+                            .launchUrl(
+                            activity,
+                            "https://www.mavcsoport.hu/mavinform?field_modalitas_value%5B%5D=vasut".toUri()
+                        )
+
+                        isMenuOpen = false
+                    },
+                    text = { Text(text = "MÁVINFORM") },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.OpenInNew,
+                            contentDescription = null
+                        )
+                    },
+                )
                 DropdownMenuItem(
                     onClick = {
                         if (Auth.currentUser == null) {
