fbd3fd6ee07f981f99a8b9927cd9e7c6c17727ed
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2025-07-07 22:55:57 +0200

    0.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.csakitheone.onrail"
         minSdk = 29
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.17.1"
+        versionCode = 20
+        versionName = "0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
